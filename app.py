e212d8e73c4b5c88fb4f3ba195619191a2f0bf7f
JJulien13 <44315008+JJulien13@users.noreply.github.com>
2025-01-16 13:42:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,6 @@ def download_file(file_type):
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=os.environ.get("PORT", 5000))
+
